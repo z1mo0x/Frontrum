d98fc7f94c59ca4abc7d5fce73bf959b98d4d8a5
z1mo0x <droweckiy@mail.ru>
2025-04-05 10:11:17 +0300

    HeaderNav decompose

--- src/components/UI/HeaderNavItem/HeaderNavItem.jsx
+++ src/components/UI/HeaderNavItem/HeaderNavItem.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export default function HeaderNavItem({ title, link }) {
     return (
         <>
-            <li>{'<NavLink>'}<NavLink to={link}>{title}</NavLink>{'</NavLink>'}</li>
+            <li>{'<NavLink> '}<NavLink to={link}>{title}</NavLink>{' </NavLink>'}</li>
         </>
     )
 }
